5ac84f05c9349dd6061b2e560db44baed3cfedd0
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2020-08-21 12:24:03 +0200

    fixed un-quoted strings

--- src/server/config.js
+++ src/server/config.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 
 const app = {
-    sessionSecret: process.env.SESSION_SECRET || TemmeligHemmelig,
+    sessionSecret: process.env.SESSION_SECRET || "TemmeligHemmelig",
     isProd: process.env.NODE_ENV === "production",
     port: process.env.PORT || 3000
 }
 
 const idporten = {
-    discoveryUrl: process.env.DISCOVERY_URL_IDPORTEN,
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    discoveryUrl: process.env.DISCOVERY_URL_IDPORTEN || "https://oidc.difi.no/idporten-oidc-provider/.well-known/openid-configuration",
+    clientID: process.env.CLIENT_ID || "bogus_client",
+    clientSecret: process.env.CLIENT_SECRET || "bogus_secret",
     redirectUri : process.env.REDIRECT_URI || "http://localhost:3000/callback",
     responseType: ['code'],
     scope: 'openid',
