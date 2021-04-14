5379395dd2608b6a4abd934b65a8f75ad9e806a2
J-K. Solbakken <jksolbakken@gmail.com>
2021-04-14 13:05:11 +0200

    debug logging

--- src/server/auth.js
+++ src/server/auth.js
@@ -52,6 +52,7 @@ export const validateOidcCallback = async (req) => {
 
 export const exchangeToken = async (session, servicename) => {
     const cachedAccessTokenSet = session[`${servicename}_accesstoken`]
+    logger.info(`session: ${Object.getOwnPropertyNames()}`)
     if (cachedAccessTokenSet && !cachedAccessTokenSet.expired()) {
         logger.info(`Using cached token for ${servicename}`)
         return Promise.resolve(cachedAccessTokenSet.access_token)
